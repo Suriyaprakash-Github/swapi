ccb3501aa1a105e67a6a1e2193e33dd031ea7743
Suriyaprakash-Github <yoursgump@gmail.com>
2023-02-17 12:54:53 +0530

    add post and delete methods

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Movie.module.css';
+import classes from "./Movie.module.css";
 
 const Movie = (props) => {
+  async function deleteMovieHandler(movie) {
+    const response = await fetch(
+      `https://react-swapi-3206d-default-rtdb.firebaseio.com/movies/${props.id}/.json`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
   return (
     <li className={classes.movie}>
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
+      <button onClick={deleteMovieHandler}>Delete Movie</button>
     </li>
   );
 };
